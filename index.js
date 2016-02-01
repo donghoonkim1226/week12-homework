14cf8574d6ce8dcb3e3b4faa731cd4a8dbf0e44f
dong hoon kim <donghoonkim1226@gmail.com>
2016-01-31 22:33:22 -0500

    finished the view function

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ var zoo = {
 		console.log("To add an animal to the zoo please fill out the following form for us!");
 		prompt.get(["->", "name", "type", "age"], function(err, result){
 			var query ="INSERT INTO animals (name, type, age) VALUES (?,?,?)";
-			var formInfo = [result.name, result.type, result.age];
-			connection.query(query, formInfo, function(err, results){
+			var animalInfo = [result.name, result.type, result.age];
+			connection.query(query, animalInfo, function(err, results){
 				if (err){
 					throw err
 				}
@@ -44,7 +44,45 @@ var zoo = {
 			});
 		});
 	},
-
+	visit: function(){
+		console.log("Enter (I): ------> do you know the animal by it's id? We will visit that animal!\n");
+		console.log("(N): ------> do you know the animal by it's name? We will visit that animal!\n");
+		console.log("(A): ------> here's the count for all animals in all locations!\n");
+		console.log("(C): ------> here's the count for all animals in this one city!\n");
+		console.log("(O): ------> here's the count for all the animals in all locations by the type you specified!\n");
+		console.log("Enter (Q): ------> Quits to the main menu!\n");
+		currentScope.visit();
+		currentScope.view(currentScope);
+	},
+	view: function(){
+		var currentScope = input_scope;
+		console.log("Please choose what you would like to visit");
+		prompt.get(["visit"]function(err, result){
+			if (result.visit === "Q"){
+				currentScope.menu();
+			} 
+			else if (result.visit === "O"){
+				currentScope.type(input_scope);
+			}
+			else if (result.type === "I"){
+				currentScope.type(input_scope);
+			}
+			else if (result.animId === "N"){
+				currentScope.name(input_scope);
+			}
+			else if (result.name === "A"){
+				currentScope.all(input_scope);
+			}
+			else if (result.all === "C"){
+				currentScope.care(input_scope);
+			}
+			else {
+				console.log("Sorry didn't get that, come again?");
+				currentScope.vist();
+				currentScope.view(currentScope);
+			}
+		});
+	},
 
 
 };
